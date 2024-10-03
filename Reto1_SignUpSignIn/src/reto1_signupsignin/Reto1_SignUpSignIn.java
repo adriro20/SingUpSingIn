11624d687a68bde89b127f0a96b65d8d27dc0829
2dam <2dam@TartangaLH.eus>
2024-10-03 12:48:52 +0200

    creacion del proyecto y la vistaLogIn empezada

---
+++ Reto1_SignUpSignIn/src/reto1_signupsignin/Reto1_SignUpSignIn.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reto1_signupsignin;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author 2dam
+ */
+public class Reto1_SignUpSignIn extends Application{
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("vistaLogIn.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * Metodo para ejecutar el programa
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
