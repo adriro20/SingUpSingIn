10478d77cf1586633ed49094a071a06922a8dfc9
2dam <2dam@TartangaLH.eus>
2024-10-04 09:36:48 +0200

    commit paquete modelo yerminado hasta lo que podemos

---
+++ Reto1_SignUpSignIn/src/modelo/UserLogIn.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author 2dam
+ */
+public class UserLogIn {
+    private String correo;
+    private String contraseña;
+
+    public UserLogIn(String correo, String contraseña) {
+        this.correo = correo;
+        this.contraseña = contraseña;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    
+    
+}
