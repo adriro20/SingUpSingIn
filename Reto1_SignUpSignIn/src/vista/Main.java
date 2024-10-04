2e99f4900bb5b8352e5b04c43539f73fdf073827
2dam <2dam@TartangaLH.eus>
2024-10-04 08:46:23 +0200

    cambio de nombres y creacion carpeta img

--- Reto1_SignUpSignIn/src/vista/Main.java
+++ Reto1_SignUpSignIn/src/vista/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package reto1_signupsignin;
+package vista;
 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
@@ -16,7 +16,7 @@ import javafx.stage.Stage;
  *
  * @author 2dam
  */
-public class Reto1_SignUpSignIn extends Application{
+public class Main extends Application{
     
     @Override
     public void start(Stage stage) throws Exception {
