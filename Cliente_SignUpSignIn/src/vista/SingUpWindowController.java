0e40c2863c8a9920648ca8d3b2299bc933f1e917
2dam <2dam@TartangaLH.eus>
2024-10-10 09:39:00 +0200

    Vista SingUp continuacion

--- Cliente_SignUpSignIn/src/vista/SingUpWindowController.java
+++ Cliente_SignUpSignIn/src/vista/SingUpWindowController.java
@@ -9,6 +9,8 @@ import java.awt.Button;
 import java.awt.TextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
@@ -33,24 +35,38 @@ public class SingUpWindowController implements  Initializable{
     TextField tfCiudad;
     
     @FXML
-    PasswordField pfContra;
+    PasswordField pfPass;
     
     @FXML
-    PasswordField pfContra2;
+    PasswordField pfPass2;
+    
+    @FXML
+    TextField tfPass;
+    
+    @FXML
+    TextField tfPass2;
     
     @FXML
     Button btnSingUp;
     
+    @FXML
+    Button btnSalir;
     
+    @FXML
+    Button btnVerPass;
+    
+    @FXML
+    Button btnVerPass2;
     
     /**
      * Metodo para coger los datos de la ventana y crear el usuario para registrar
      */
-    private void singUp(){
+    @FXML
+    private void singUp(ActionEvent event){
           if(tfNombre.getText().equals("") || tfTelefono.getText().equals("") || tfCorreo.getText().equals("")
-                || tfCiudad.getText().equals("") || pfContra.getText().equals("") || pfContra2.getText().equals("")){
+                || tfCiudad.getText().equals("") || pfPass.getText().equals("") || pfPass2.getText().equals("")){
             new Alert(Alert.AlertType.ERROR, "Te Falta algun campo por rellenar");
-        }else if(pfContra.getText().equalsIgnoreCase(pfContra2.getText())){
+        }else if(pfPass.getText().equalsIgnoreCase(pfPass2.getText())){
             new Alert(Alert.AlertType.ERROR, "La contraseña no es igual en los dos campos");
         }else if(tfTelefono.getText().length()!=8){
             new Alert(Alert.AlertType.ERROR, "El numero de telefono tiene que tener 8 digitos");
@@ -66,17 +82,53 @@ public class SingUpWindowController implements  Initializable{
         }
     }
     
-    /**
-     * Metodo para cerrar la aplicacion
-     */
-    private void salir(){
-        
+     @FXML
+    private void salir(ActionEvent event){
+        Platform.exit();
     }
+    
+    /**
+     * metodo para que sea visible la contraseña repetida
+     * @param event 
+     */
+    @FXML
+    private void verPass(ActionEvent event) {
+        if(tfPass.isVisible()){
+            pfPass.setText(tfPass.getText());            
+            pfPass.setVisible(true);
+            tfPass.setVisible(false);
+        }else{
+            tfPass.setText(pfPass.getText());
+            tfPass.setVisible(true);
+            pfPass.setVisible(false);           
+        }
+    }
+    
+    /**
+     * metodo para que sea visible la contraseña repetida
+     * @param event 
+     */
+    @FXML
+    private void verPass2(ActionEvent event) {
+        if(tfPass2.isVisible()){
+            pfPass2.setText(tfPass2.getText());            
+            pfPass2.setVisible(true);
+            tfPass2.setVisible(false);
+        }else{
+            tfPass2.setText(pfPass2.getText());
+            tfPass2.setVisible(true);
+            pfPass2.setVisible(false);           
+        }
+    }
+    
+    
+   
     
     /**
      * Metodo para ir a la vista Sing in
      */
-    private void irSingIn(){
+    @FXML
+    private void irSingIn(ActionEvent event){
         
     }
     
