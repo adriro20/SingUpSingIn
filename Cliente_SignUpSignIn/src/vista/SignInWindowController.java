9211be994d55656be33460fd9aa5dbdff46e7b39
2dam <2dam@TartangaLH.eus>
2024-10-22 08:34:32 +0200

    terminado javadoc en el controlador signIn

--- Cliente_SignUpSignIn/src/vista/SignInWindowController.java
+++ Cliente_SignUpSignIn/src/vista/SignInWindowController.java
@@ -39,12 +39,16 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
- * Clase que controla la ventana de inicio de sesión (LogInWindow). Proporciona
- * métodos para manejar las acciones de los controles en la interfaz de usuario
- * como iniciar sesión, cambiar a la ventana de registro, mostrar u ocultar
- * contraseñas, y salir de la aplicación.
+ * Controlador de la ventana de inicio de sesión (viewSignIn).
  *
- * @author 2dam
+ * Esta clase gestiona la interacción de los controles de la interfaz gráfica
+ * con el usuario, como iniciar sesión, alternar la visibilidad de la
+ * contraseña, cambiar a la ventana de registro y salir de la aplicación.
+ *
+ * Implementa la interfaz Initializable para inicializar los componentes de la
+ * interfaz creados desde un archivo FXML.
+ *
+ * @author Julen Hidalgo
  */
 public class SignInWindowController implements Initializable {
 
@@ -104,61 +108,94 @@ public class SignInWindowController implements Initializable {
     @FXML
     TextField tfPass;
 
+    /**
+     * Panel principal que contiene los elementos de la interfaz.
+     */
     @FXML
     BorderPane bpPrincipal;
 
     /**
      * Cierra la aplicación.
      *
+     * Solicita confirmación del usuario antes de cerrar la aplicación.
+     *
      * @param event Evento que se dispara cuando el usuario hace clic en el
      * botón "Salir".
      */
     private void closeApp(ActionEvent event) {
+        //Se muestra un Alert con dos opciones para confirmar que el usuario 
+        //quiere cerrar la app.
         Optional<ButtonType> confirmar = new Alert(Alert.AlertType.CONFIRMATION, "¿Está seguro de que desea salir?", ButtonType.YES, ButtonType.NO).showAndWait();
+
+        //Si la confirmación del Alert es verdadera, se cierra el programa.
         if (confirmar.get() == ButtonType.YES) {
             Platform.exit();
         }
     }
 
     /**
-     * Método para manejar el inicio de sesión. Valida que el campo de correo y
-     * el de contraseña estén llenos antes de proceder con el inicio de sesión.
-     * Si falta algún campo, se muestra una alerta de error.
+     * Maneja el proceso de inicio de sesión.
      *
-     * @param event Evento que se dispara cuando el usuario hace clic en el
-     * botón "Sign In".
+     * Verifica que los campos de correo y contraseña no estén vacíos y envía
+     * una solicitud de inicio de sesión. Si la autenticación es exitosa, cambia
+     * a la ventana principal. Si falla, muestra un mensaje de error dependiendo
+     * de el tipo de error que haya sido.
+     *
+     * @param event Evento disparado al hacer clic en el botón "Sign In".
      */
     private void signIn(ActionEvent event) {
+        //Se crea un objeto de la clase Message.
         Message mensaje = new Message();
+
+        //Se comprueba que los campos no estén vacios.
         if (tfCorreo.getText().equals("") || pfPass.getText().equals("")) {
-            new Alert(Alert.AlertType.ERROR, "Todos los campos tienen que estar llenos", ButtonType.OK).showAndWait();
+            //En el caso de que algún campo esté vacío se manda un Alert con el 
+            //texto "Los campos no pueden estar vacíos".
+            new Alert(Alert.AlertType.ERROR, "Los campos no pueden estar vacíos", ButtonType.OK).showAndWait();
         } else {
+            //En el caso de que no haya ningún campo vacío se crea un nuevo 
+            //objeto de la clase User y se le añaden los atributos Email y 
+            //Password con los valores introducidos en los campos.
             User user = new User();
             user.setEmail(tfCorreo.getText());
             user.setPassword(pfPass.getText());
 
+            //Se añade el User creado al Message.
             mensaje.setUser(user);
+
+            //Se le añade al Message el Request SING_IN_REQUEST.
             mensaje.setRequest(Request.SING_IN_REQUEST);
 
             try {
-                if (SignableFactory.getSignable().signIn(mensaje)) {
-                    FXMLLoader loader = new FXMLLoader(getClass().getResource("viewSignOut.fxml"));
-                    Parent root = loader.load();
+                // Se manda el Message creado al servidor, en caso de que no 
+                // salte ninguna excepción significa que todo ha ido correctamente.
+                SignableFactory.getSignable().signIn(mensaje);
 
-                    // Obtener el Stage desde el nodo que disparó el evento
-                    Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+                // Se carga el FXML con la información de la vista viewSignOut.
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("viewSignOut.fxml"));
+                Parent root = loader.load();
 
-                    // Crear una nueva escena con el contenido cargado
-                    Scene scene = new Scene(root);
+                // Obtener el Stage desde el nodo que disparó el evento.
+                Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
 
-                    // Establecer la nueva escena en el Stage
-                    stage.setScene(scene);
-                    stage.show();
-                }
+                // Se crea un nuevo objeto de la clase Scene con el FXML cargado.
+                Scene scene = new Scene(root);
+
+                // Se muestra en la ventana el Scene creado.
+                stage.setScene(scene);
+                stage.show();
+
             } catch (IOException ex) {
+                // Si salta una IOException significa que ha habido algún 
+                // problema al cargar el FXML o al intentar llamar a la nueva 
+                // ventana, por lo que se mostrará un Alert con el mensaje 
+                // "Error en la sincronización de ventanas, intentalo más tarde".
                 Logger.getLogger(SignInWindowController.class.getName()).log(Level.SEVERE, null, ex);
-                new Alert(Alert.AlertType.ERROR, "Error en la sincronización de ventanas, intentalo más tarde.", ButtonType.OK).showAndWait();
+                new Alert(Alert.AlertType.ERROR, "Error en la sincronización de ventanas, intentalo más tarde", ButtonType.OK).showAndWait();
             } catch (InternalServerErrorException | LogInDataException | NoConnectionsAvailableException ex) {
+                // Si salta alguna de las excepciones creadas por nosotros se 
+                // muestra un Alert con el mensaje correspondiente de 
+                // cada una de ellas.
                 Logger.getLogger(SignInWindowController.class.getName()).log(Level.SEVERE, null, ex);
                 new Alert(Alert.AlertType.ERROR, ex.getMessage(), ButtonType.OK).showAndWait();
             }
@@ -166,51 +203,61 @@ public class SignInWindowController implements Initializable {
     }
 
     /**
-     * Cambia a la ventana de registro (SignUpWindow). Carga la vista
-     * correspondiente al registro y la establece en la escena actual.
+     * Cambia a la ventana de registro (viewSignUp).
      *
-     * @param event Evento que se dispara cuando el usuario hace clic en el
-     * enlace "Sign Up".
-     * @throws IOException Si ocurre un error al cargar el archivo FXML de la
-     * vista de Sign Up.
+     * @param event Evento disparado al hacer clic en el enlace "Sign Up".
      */
     private void signUp(ActionEvent event) {
         try {
+            // Se carga el FXML con la información de la vista viewSignUp.
             FXMLLoader loader = new FXMLLoader(getClass().getResource("viewSignUp.fxml"));
             Parent root = loader.load();
 
-            // Obtener el Stage desde el nodo que disparó el evento
+            // Obtener el Stage desde el nodo que disparó el evento.
             Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
 
-            // Crear una nueva escena con el contenido cargado
+            // Se crea un nuevo objeto de la clase Scene con el FXML cargado.
             Scene scene = new Scene(root);
 
-            // Establecer la nueva escena en el Stage
+            // Se muestra en la ventana el Scene creado.
             stage.setScene(scene);
             stage.show();
         } catch (IOException ex) {
+            // Si salta una IOException significa que ha habido algún 
+            // problema al cargar el FXML o al intentar llamar a la nueva 
+            // ventana, por lo que se mostrará un Alert con el mensaje 
+            // "Error en la sincronización de ventanas, intentalo más tarde".
             Logger.getLogger(SignInWindowController.class.getName()).log(Level.SEVERE, null, ex);
             new Alert(Alert.AlertType.ERROR, "Error en la sincronización de ventanas, intentalo más tarde.", ButtonType.OK).showAndWait();
         }
     }
 
     /**
-     * Alterna la visibilidad de la contraseña (pfPass/tfPass). Si el campo de
-     * texto de la contraseña está visible, lo oculta y muestra el campo de
-     * password; si no, muestra la contraseña en texto plano.
+     * Alterna la visibilidad de la contraseña (pfPass/tfPass). Si el PasswordField 
+     * está visible, la contraseña no se puede leer, en cambio si el campo que 
+     * está visible es el TextField la contraseña tiene formato de texto plano.
      *
      * @param event Evento que se dispara cuando el usuario hace clic en el
      * botón "Ver Pass".
      */
     private void verPass(ActionEvent event) {
+        //Se obtiene el estilo del botón VerPass.
         String estilo = btnVerPass.getStyle();
+        
+        //Se quita la imagen del fondo del botón.
         String estiloNuevo = estilo.replace("-fx-background-image: url\\('.*'\\);", "");
+        
+        //Se comprueba si el PasswordField o el TextField es visible.
         if (tfPass.isVisible()) {
+            //Si el TextField está visible, se alterna la visibilidad, se copia 
+            //el texto en el PasswordField y se cambia la imagen de fondo.
             pfPass.setText(tfPass.getText());
             pfPass.setVisible(true);
             tfPass.setVisible(false);
             btnVerPass.setStyle(estiloNuevo + "-fx-background-image: url('/img/iconoOjoAbierto.png');");
         } else {
+            //Si el PasswordField está visible, se alterna la visibilidad, se copia 
+            //el texto en el PasswordField y se cambia la imagen de fondo.
             tfPass.setText(pfPass.getText());
             tfPass.setVisible(true);
             pfPass.setVisible(false);
@@ -218,40 +265,55 @@ public class SignInWindowController implements Initializable {
         }
     }
 
+    /**
+     * Metodo para abrir el menú conceptual en el caso de que se haga clic con 
+     * el botón derecho del ratón, en cambio, si se hace clic con cualqier otro 
+     * botón del raton se cierra el menú.
+     * 
+     * @param event Es el evento que compureba que botón del ratón se clica.
+     * @param menu Es el menú contextual que se muestra.
+     */
     private void controlMenuConceptual(MouseEvent event, ContextMenu menu) {
+        //Se comprueba si se hace clic con el borón derecho del ratón.
         if (event.getButton() == MouseButton.SECONDARY) {
+            //Si es así se abre el menú contextual.
             menu.show(bpPrincipal, event.getScreenX(), event.getScreenY());
         } else {
+            //Si no, se cierra el mismo.
             menu.hide();
         }
     }
 
     /**
-     * Inicializa la ventana de inicio de sesión cuando ésta es creada.
+     * Es el metodo qeu inicializa la ventana de inicio de sesión, además es la 
+     * que le da las propiedades de recoger eventos a todos los botones.
      *
      * @param location Ubicación del archivo FXML utilizado para crear la
      * interfaz.
-     * @param resources Recursos internacionales utilizados para la interfaz.
+     * @param resources Recursos utilizados para la interfaz.
      */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        //Se le quita la propiedad Resizable a la ventana y se le añade el título.
         Platform.runLater(() -> {
             Stage stage = (Stage) bpPrincipal.getScene().getWindow();
             stage.setResizable(false);
             stage.setTitle("Inicio de sesión");
         });
 
+        //Se añaden los listeners a todos los botones.
         btnSalir.setOnAction(this::closeApp);
         btnVerPass.setOnAction(this::verPass);
         btnSignIn.setOnAction(this::signIn);
         hlSignUp.setOnAction(this::signUp);
         hlCrear.setOnAction(this::signUp);
 
+        //Se crea el menú contextual, el cual se mostrará si se hace clic con el 
+        //botón izquierdo del ratón.
         ContextMenu contextMenu = new ContextMenu();
         MenuItem item1 = new MenuItem("Cambiar a tema oscuro");
         MenuItem item2 = new MenuItem("Cambiar a tema claro");
         contextMenu.getItems().addAll(item1, item2);
-
         bpPrincipal.setOnMouseClicked(event -> controlMenuConceptual(event, contextMenu));
 
     }
